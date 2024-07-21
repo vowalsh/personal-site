d0a28ad043f79d2a9ee09435d06cdb4346ad0018
V. Oliver Walsh <oliverwalsh7@gmail.com>
2024-07-21 11:47:27 -0400

    optimize for mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -93,61 +93,30 @@
     .section-visible .section-content {
         opacity: 1;
     }
-
-    .timeline-line {
-        height: calc(100% + 2.75rem);
-    }
-
-    @media (min-width: 640px) {
-        .timeline-line {
-            height: calc(100% + 3rem);
-        }
-    }
-
-    .timeline-item:last-child .timeline-line {
-        height: 0;
-    }
-
-    .timeline-item:not(:last-child) .timeline-line {
-        height: calc(100% + 3rem);
-    }
-
 </style>
 
 <Header {currentSection} />
 
-<main class="bg-terminal-black text-terminal-white font-mono min-h-screen overflow-y-auto text-sm sm:text-base">
+<main class="bg-terminal-black text-terminal-white font-mono min-h-screen overflow-y-auto text-xs sm:text-sm md:text-base">
     <!-- home -->
-    <section id="owal" class="min-h-screen flex items-center justify-center p-4 sm:p-8" class:section-visible={sectionVisibility['owal']}>
-        <div class="section-content w-full max-w-5xl px-4 sm:px-6 lg:px-8 flex justify-center items-center">
+    <section id="owal" class="min-h-screen flex items-center justify-center p-2 sm:p-4 md:p-8" class:section-visible={sectionVisibility['owal']}>
+        <div class="section-content w-full max-w-5xl px-2 sm:px-4 md:px-8 flex justify-center items-center">
             <div class="w-full sm:w-5/6 md:w-4/5 lg:w-3/4 min-h-[25vh] text-center">
-                <h1 class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold mb-4">
+                <h1 class="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold mb-2 sm:mb-4">
                     <TerminalText text="v. oliver walsh" speed={100} showCursor={true} cursorColor="transparent" />
                 </h1>
-                <div class="space-y-4">
+                <div class="space-y-2 sm:space-y-4">
                     {#if showSubheader}
-                    <p class="text-lg sm:text-xl md:text-2xl">
+                    <p class="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
                         <TerminalText text="machine learning engineering" speed={50} showCursor={false} />
                     </p>
-                    {/if}
-                </div>
-                <div class="space-y-4">
-                    {#if showSubheader}
-                    <p class="text-lg sm:text-xl md:text-2xl">
+                    <p class="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
                         <TerminalText text="blockchain engineering" speed={75} showCursor={false} />
                     </p>
-                    {/if}
-                </div>
-                <div class="space-y-4">
-                    {#if showSubheader}
-                    <p class="text-lg sm:text-xl md:text-2xl">
+                    <p class="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
                         <TerminalText text="distributed systems" speed={100} showCursor={false} />
                     </p>
-                    {/if}
-                </div>
-                <div class="space-y-4">
-                    {#if showSubheader}
-                    <p class="text-lg sm:text-xl md:text-2xl">
+                    <p class="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
                         <TerminalText text="decentralization" speed={125} showCursor={false} />
                     </p>
                     {/if}
@@ -157,110 +126,100 @@
     </section>
 
     <!-- about -->
-    <section id="about" class="flex items-center justify-center bg-terminal-black text-terminal-white p-8" class:section-visible={sectionVisibility['about']}>
+    <section id="about" class="flex items-center justify-center bg-terminal-black text-terminal-white p-4 sm:p-8" class:section-visible={sectionVisibility['about']}>
         <div class="section-content w-full max-w-3xl text-center">
-            <h2 class="text-3xl font-bold mb-8">about me</h2>
-            <img src="/pic.png" alt="owal" class="zoomed-image w-32 sm:w-36 md:w-40 lg:w-48 aspect-square object-cover object-center rounded-full mx-auto mb-2 border border-terminal-white">
-            <p class="text-sm italic mb-6 mt-6">lookin' smug in nyc</p>
-            <p class="text-lg mb-4">
+            <h2 class="text-xl sm:text-2xl md:text-3xl font-bold mb-4 sm:mb-8">about me</h2>
+            <img src="/pic.png" alt="owal" class="zoomed-image w-24 sm:w-32 md:w-40 lg:w-48 aspect-square object-cover object-center rounded-full mx-auto mb-2 border border-terminal-white">
+            <p class="text-xs sm:text-sm italic mb-3 sm:mb-6 mt-3 sm:mt-6">lookin' smug in nyc</p>
+            <p class="text-xs sm:text-sm md:text-base lg:text-lg mb-2 sm:mb-4">
                 i'm a machine learning engineer currently specializing in blockchain integration. my passion lies in developing innovative solutions that seamlessly incorporate ml into decentralized systems and other highly technical domains.</p>
-            <p class="text-lg">
+            <p class="text-xs sm:text-sm md:text-base lg:text-lg">
                 with experience in big tech, startup, and research environments, i strive to push the boundaries of what's possible in the realm of ai-powered blockchain applications.
             </p>
         </div>
     </section>
       
     <!-- timeline -->
-    <section id="timeline" class="min-h-screen flex items-center justify-center bg-terminal-black text-terminal-white p-4 sm:p-8" class:section-visible={sectionVisibility['timeline']}>
+    <section id="timeline" class="min-h-screen flex items-center justify-center bg-terminal-black text-terminal-white p-2 sm:p-4 md:p-8" class:section-visible={sectionVisibility['timeline']}>
         <div class="section-content w-full max-w-3xl relative overflow-y-auto max-h-[80vh]">
-            <h2 class="text-2xl sm:text-3xl font-bold mb-8 sm:mb-12 text-center sticky top-0 bg-terminal-black py-4 z-10">timeline</h2>
-            <div class="relative mt-8 sm:mt-16">
+            <h2 class="text-xl sm:text-2xl md:text-3xl font-bold mb-4 sm:mb-8 md:mb-12 text-center sticky top-0 bg-terminal-black py-2 sm:py-4 z-10">timeline</h2>
+            <div class="relative mt-4 sm:mt-8 md:mt-16">
                 {#each timelineEvents as event, index}
-                <div class="mb-6 sm:mb-8 relative flex cursor-pointer timeline-item" on:click={() => toggleEvent(event)} on:keydown={(e) => e.key === 'Enter' && toggleEvent(event)} role="button" tabindex="0">
-                    <div class="w-16 sm:w-24 text-right pr-2 sm:pr-4 font-bold text-sm sm:text-base flex-shrink-0" class:text-[#94cbf3]={activeEvents.has(event)}>{event.year}</div>
-                    <div class="ml-2 sm:ml-4 flex-grow relative">
-                        <div class="flex items-start">
-                            <div class="w-2 h-2 sm:w-3 sm:h-3 rounded-full bg-terminal-white mt-1 flex-shrink-0 relative">
-                                {#if index !== timelineEvents.length - 1}
-                                {/if}
+                <div class="mb-3 sm:mb-6 md:mb-8 relative flex items-center cursor-pointer timeline-item" on:click={() => toggleEvent(event)} on:keydown={(e) => e.key === 'Enter' && toggleEvent(event)} role="button" tabindex="0">
+                    <div class="w-12 sm:w-16 md:w-24 text-right pr-1 sm:pr-2 md:pr-4 font-bold text-xs sm:text-sm md:text-base flex-shrink-0" class:text-[#94cbf3]={activeEvents.has(event)}>{event.year}</div>
+                    <div class="ml-1 sm:ml-2 md:ml-4 flex-grow relative">
+                        <div class="flex items-center">
+                            <span class="text-terminal-white text-sm sm:text-base md:text-lg mr-2">*</span>
+                            <div class="text-xs sm:text-sm md:text-base" class:text-[#94cbf3]={activeEvents.has(event)}>
+                                {event.description}
                             </div>
-                                <div class="ml-2 sm:ml-4 text-sm sm:text-base" class:text-[#94cbf3]={activeEvents.has(event)}>
-                                    {event.description}
-                                </div>
-                            </div>
-                            {#if activeEvents.has(event)}
-                                <div class="mt-2 sm:mt-4 ml-5 sm:ml-8 pr-2 sm:pr-4" transition:fade={{ duration: 300 }}>
-                                    <div class="italic text-xs sm:text-sm">{event.fullTimeline}</div>
-                                    {#if event.organizations && event.organizations.length > 0}
-                                        <div class="mt-1 sm:mt-2 text-xs sm:text-sm">
-                                            {@html event.organizations.map(org => `<a href="${org.link}" target="_blank" rel="noopener noreferrer" class="underline hover:text-gray-300">${org.name}</a>`).join(', ')}
-                                        </div>
-                                    {/if}
-                                    <div class="mt-1 sm:mt-2 text-xs sm:text-sm whitespace-pre-line">{@html event.details.replace(/\n/g, '<br>')}</div>
-                                </div>
-                            {/if}
                         </div>
+                        {#if activeEvents.has(event)}
+                            <div class="mt-1 sm:mt-2 md:mt-4 ml-2 sm:ml-4 md:ml-8 pr-1 sm:pr-2 md:pr-4" transition:fade={{ duration: 300 }}>
+                                <div class="italic text-xxs sm:text-xs md:text-sm">{event.fullTimeline}</div>
+                                {#if event.organizations && event.organizations.length > 0}
+                                    <div class="mt-0.5 sm:mt-1 md:mt-2 text-xxs sm:text-xs md:text-sm">
+                                        {@html event.organizations.map(org => `<a href="${org.link}" target="_blank" rel="noopener noreferrer" class="underline hover:text-gray-300">${org.name}</a>`).join(', ')}
+                                    </div>
+                                {/if}
+                                <div class="mt-0.5 sm:mt-1 md:mt-2 text-xxs sm:text-xs md:text-sm whitespace-pre-line">{@html event.details.replace(/\n/g, '<br>')}</div>
+                            </div>
+                        {/if}
                     </div>
+                </div>
                 {/each}
             </div>
         </div>
     </section>
       
-
     <!-- blog -->
-    <section id="thoughts" class="flex items-center justify-center bg-terminal-black text-terminal-white p-8" class:section-visible={sectionVisibility['thoughts']}>
+    <section id="thoughts" class="flex items-center justify-center bg-terminal-black text-terminal-white p-4 sm:p-8" class:section-visible={sectionVisibility['thoughts']}>
         <div class="section-content w-full max-w-3xl">
-            <h2 class="text-3xl font-bold mb-12 text-center">thoughts</h2>
+            <h2 class="text-xl sm:text-2xl md:text-3xl font-bold mb-6 sm:mb-12 text-center">thoughts</h2>
             <div class="relative">
                 {#each blogPosts as post, index}
-                <div class="mb-8">
+                <div class="mb-4 sm:mb-8">
                     <div class="flex items-center cursor-pointer" on:click={() => toggleBlogPost(post)}>
-                            {#if !activeBlogPosts.has(post)}
-                                <div class="w-2 h-2 sm:w-3 sm:h-3 rounded-full bg-terminal-white relative">
-                                    {#if index !== blogPosts.length - 1 && blogPosts.length > 1}
-                                        <div class="absolute left-1/2 top-full w-0.5 bg-terminal-white transform -translate-x-1/2" style="height: calc(100% + 1.5rem);"></div>
-                                    {/if}
-                                </div>
-                            {/if}
-
-                            <div class="ml-4 flex-grow">
-                                <div class="flex justify-between items-baseline">
-                                    <h3 class="text-lg sm:text-xl font-bold" class:text-[#94cbf3]={activeBlogPosts.has(post)}>{post.title}</h3>
-                                    <span class="text-sm italic">{post.date}</span>
-                                </div>
-                                {#if activeBlogPosts.has(post)}
-                                    {#if post.image && post.image.trim() !== ''}
-                                        <img src={post.image} alt={post.title} class="w-full h-48 object-cover my-4 rounded">
-                                    {/if}
-                                    <p class="mt-2 text-xs whitespace-pre-line">{@html post.content}</p>
-                                {/if}
+                        {#if !activeBlogPosts.has(post)}
+                            <span class="text-terminal-white text-sm sm:text-base md:text-lg mr-2">*</span>
+                        {/if}
+                        <div class="flex-grow">
+                            <div class="flex justify-between items-baseline">
+                                <h3 class="text-sm sm:text-base md:text-lg lg:text-xl font-bold" class:text-[#94cbf3]={activeBlogPosts.has(post)}>{post.title}</h3>
+                                <span class="text-xxs sm:text-xs md:text-sm italic">{post.date}</span>
                             </div>
-                            
+                            {#if activeBlogPosts.has(post)}
+                                {#if post.image && post.image.trim() !== ''}
+                                    <img src={post.image} alt={post.title} class="w-full h-32 sm:h-48 object-cover my-2 sm:my-4 rounded">
+                                {/if}
+                                <p class="mt-1 sm:mt-2 text-xxs sm:text-xs md:text-sm whitespace-pre-line">{@html post.content}</p>
+                            {/if}
                         </div>
                     </div>
+                </div>
                 {/each}
             </div>
         </div>
     </section>
 
     <!-- contact -->
-    <section id="contact" class="flex items-center justify-center bg-terminal-black text-terminal-white p-8" class:section-visible={sectionVisibility['contact']}>
+    <section id="contact" class="flex items-center justify-center bg-terminal-black text-terminal-white p-4 sm:p-8" class:section-visible={sectionVisibility['contact']}>
         <div class="section-content w-full max-w-3xl text-center">
-            <h2 class="text-3xl font-bold mb-8">contact</h2>
-            <p class="mb-4">feel free to reach out:</p>
-            <a href="mailto:oliverwalsh7@gmail.com" class="text-[#94cbf3] hover:underline">inquire@owal.me</a>
-            <div class="mt-6 flex justify-center space-x-4">
-                <a href="https://github.com/oliverwalsh7" target="_blank" rel="noopener noreferrer" class="text-[#94cbf3] hover:underline">github</a>
-                <a href="https://linkedin.com/in/owal" target="_blank" rel="noopener noreferrer" class="text-[#94cbf3] hover:underline">linkedin</a>
-                <a href="https://twitter.com/0xOwal" target="_blank" rel="noopener noreferrer" class="text-[#94cbf3] hover:underline">twitter</a>
+            <h2 class="text-xl sm:text-2xl md:text-3xl font-bold mb-4 sm:mb-8">contact</h2>
+            <p class="text-xs sm:text-sm md:text-base mb-2 sm:mb-4">feel free to reach out:</p>
+            <a href="mailto:inquire@owal.me" class="text-[#94cbf3] hover:underline text-xs sm:text-sm md:text-base">inquire@owal.me</a>
+            <div class="mt-3 sm:mt-6 flex justify-center space-x-2 sm:space-x-4">
+                <a href="https://github.com/oliverwalsh7" target="_blank" rel="noopener noreferrer" class="text-[#94cbf3] hover:underline text-xs sm:text-sm md:text-base">github</a>
+                <a href="https://linkedin.com/in/owal" target="_blank" rel="noopener noreferrer" class="text-[#94cbf3] hover:underline text-xs sm:text-sm md:text-base">linkedin</a>
+                <a href="https://twitter.com/0xOwal" target="_blank" rel="noopener noreferrer" class="text-[#94cbf3] hover:underline text-xs sm:text-sm md:text-base">twitter</a>
             </div>
         </div>
     </section>
     
     <!-- footer -->
-    <footer class="bg-terminal-black text-terminal-white text-center py-2 fixed bottom-0 left-0 right-0 z-50">
-        <p class="text-xs">&copy; {new Date().getFullYear()} v. oliver walsh. all rights reserved.</p>
-        <p class="text-xs mt-2">last updated: 7/21/2024</p>
+    <footer class="bg-terminal-black text-terminal-white text-center py-1 sm:py-2 fixed bottom-0 left-0 right-0 z-50">
+        <p class="text-xxs sm:text-xs">&copy; {new Date().getFullYear()} v. oliver walsh. all rights reserved.</p>
+        <p class="text-xxs sm:text-xs mt-1 sm:mt-2">last updated: 7/21/2024</p>
     </footer>
     
 </main>
